1a0164da0a70c4af8e63449d37965c270d6716ad
Quentin Brault <tititesouris@laposte.net>
2015-09-19 17:19:00 +0200

    PHP to change turtle size

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
+<?php
+    $turtle_size = 5;
+?>
 <!DOCTYPE html>
     <html lang="fr">
-
     <head>
         <title>Pedacogite</title>
 
         <meta charset="utf-8" />
-        <meta name="description" content="Pedacogite" />
+        <meta name="description" content="Pedacogite is an interactive way to learn programming with Turtle." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link href="./css/materialize.min.css" rel="stylesheet" media="all" />
@@ -16,15 +18,15 @@
 
     <body>
         <div id="canvas" style="width: 600px; height: 600px; border: 1px solid black;">
-            <svg x="0px" y="0px" version="1.1" baseProfile="full" width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" fill="#d2b3c5">
+            <svg x="0px" y="0px" version="1.1" baseProfile="full" width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
                 <g id="turtle" transform="translate(500,500) rotate(0)" x="500" y="500" angle="0">
-                    <circle cx="10" cy="0" r="5" fill="#33dd44" stroke="#544a2c" stroke-width="2" />
-                    <circle cx="0" cy="0" r="10" fill="#544a2c" stroke="#242a0c" stroke-width="2" />
-                    <circle cx="-4" cy="-4" r="2" fill="#ad953e" stroke="#8d650e" stroke-width="1" />
-                    <circle cx="-4" cy="4" r="2" fill="#ad953e" stroke="#8d650e" stroke-width="1" />
-                    <circle cx="4" cy="-4" r="2" fill="#ad953e" stroke="#8d650e" stroke-width="1" />
-                    <circle cx="4" cy="4" r="2" fill="#ad953e" stroke="#8d650e" stroke-width="1" />
-                    <circle cx="0" cy="0" r="2" fill="#ad953e" stroke="#8d650e" stroke-width="1" />
+                    <circle cx="<?php echo 2 * $turtle_size; ?>" cy="0" r="<?php echo $turtle_size; ?>" fill="#33dd44" stroke="#544a2c" stroke-width="2" />
+                    <circle cx="0" cy="0" r="<?php echo 2 * $turtle_size; ?>" fill="#544a2c" stroke="#242a0c" stroke-width="2" />
+                    <circle cx="-<?php echo $turtle_size - 2; ?>" cy="-<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
+                    <circle cx="-<?php echo $turtle_size - 2; ?>" cy="<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
+                    <circle cx="<?php echo $turtle_size - 2; ?>" cy="-<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
+                    <circle cx="<?php echo $turtle_size - 2; ?>" cy="<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
+                    <circle cx="0" cy="0" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
                 </g>
             </svg>
         </div>
