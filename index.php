d8cf90f38aaf3f05646815b06f9962fcff5ca24f
Quentin Brault <osberbot@gmail.com>
2015-09-26 00:13:50 +0200

    Starting over with Skulpt

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $turtle_size = 5;
 ?>
 <!DOCTYPE html>
-    <html lang="fr" style="height: 90%;">
+    <html lang="fr">
     <head>
         <title>Pedacogite</title>
 
@@ -16,40 +16,24 @@
 
     </head>
 
-    <body style="height: 100%;" class="orange lighten-4">
-        <br />
-        <div class="row" style="height: 100%;">
-            <div class="col s6 m6 l6" style="height: 100%;">
-                <div id="canvas">
-                    <svg x="0px" y="0px" version="1.1" baseProfile="full" width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-                        <g id="turtle" transform="translate(500,500) rotate(0)" x="500" y="500" angle="0" pen="true" speed="0.1" pause="false">
-                            <circle cx="<?php echo 2 * $turtle_size; ?>" cy="0" r="<?php echo $turtle_size; ?>" fill="#538a44" stroke="#544a2c" stroke-width="2" />
-                            <circle cx="0" cy="0" r="<?php echo 2 * $turtle_size; ?>" fill="#544a2c" stroke="#242a0c" stroke-width="2" />
-                            <circle cx="-<?php echo $turtle_size - 2; ?>" cy="-<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
-                            <circle cx="-<?php echo $turtle_size - 2; ?>" cy="<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
-                            <circle cx="<?php echo $turtle_size - 2; ?>" cy="-<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
-                            <circle cx="<?php echo $turtle_size - 2; ?>" cy="<?php echo $turtle_size - 2; ?>" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
-                            <circle cx="0" cy="0" r="<?php echo $turtle_size / 2; ?>" fill="#ad953e" stroke="#8d650e" stroke-width="<?php echo $turtle_size / 5; ?>" />
-                        </g>
-                    </svg>
-                </div>
-                <div id="button-box">
-                    <a id="play-btn" class="btn-floating btn-large green"><i class="material-icons">play_arrow</i></a>
-                    <a id="pause-btn" class="btn-floating btn-large orange darken-3" style="display: none;"><i class="material-icons">pause</i></a>
-                    <a id="stop-btn" class="btn-floating btn-large red"><i class="material-icons">stop</i></a>
-                </div>
-            </div>
-            <div class="col s6 m6 l6" style="height: 100%;">
-                <textarea id="script-input">print("Bonjour le monde!")&#13;&#10;move(50)&#13;&#10;turn(50)&#13;&#10;move(50)&#13;&#10;penUp()&#13;&#10;move(50)&#13;&#10;penDown()&#13;&#10;move(100)</textarea>
-                <div id="script-output">-- TurtleScript v1.0 --</div>
-            </div>
-        </div>
-                <div id="export-box">[]</div>
+    <body>
+        
+        <form>
+            <textarea id="yourcode" cols="40" rows="10">import turtle&#13;&#10;t = turtle.Turtle()&#13;&#10;t.forward(100)&#13;&#10;print "Hello World"</textarea>
+            <br />
+            <button type="button" id="run-btn">Run</button>
+        </form>
+        
+        <pre id="output" ></pre>
+        
+        <div id="canvas"></div>
+        
         <footer>
-            <script src="./js/jquery-1.11.3.min.js"></script>
-            <script src="./js/materialize.min.js"></script>
-            <script src="./js/turtle.js"></script>
-            <script src="./js/parser.js"></script>
+            <script src="./js/jquery-1.11.3.min.js" type="text/javascript"></script>
+            <script src="./js/materialize.min.js" type="text/javascript"></script>
+            <script src="./js/skulpt.min.js" type="text/javascript"></script>
+            <script src="./js/skulpt-stdlib.js" type="text/javascript"></script>
+            <script src="./js/website.js" type="text/javascript"></script>
         </footer>
     </body>
 </html>
---
+++ js/website.js
@@ -0,0 +1,29 @@
+function outf(text) { 
+    var mypre = document.getElementById("output"); 
+    mypre.innerHTML = mypre.innerHTML + text; 
+} 
+
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+$(function() {
+    $('#run-btn').click(function() {
+        var prog = $('#yourcode').text();
+        var mypre = $('#output');
+        mypre.innerHTML = '';
+        Sk.pre = "output";
+        Sk.configure({output:outf, read:builtinRead});
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'canvas';
+        var myPromise = Sk.misceval.asyncToPromise(function() {
+            return Sk.importMainWithBody("<stdin>", false, prog, true);
+        });
+        myPromise.then(function(mod) {
+            console.log('success');
+        }, function(err) {
+            console.log(err.toString());
+        });
+    });
+});
